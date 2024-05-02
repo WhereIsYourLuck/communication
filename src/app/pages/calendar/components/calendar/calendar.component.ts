62faecbcca8a684af8d1c3383696acf7354f3f30
rlesage71 <97740871+rlesage71@users.noreply.github.com>
2024-05-02 14:26:54 +0200

    Tentative de récupération depuis la BDD mais ca marche pas

--- src/app/pages/calendar/components/calendar/calendar.component.ts
+++ src/app/pages/calendar/components/calendar/calendar.component.ts
@@ -3,11 +3,14 @@ import { FullCalendarModule } from "@fullcalendar/angular";
 import { CommonModule } from "@angular/common";
 import { CalendarOptions } from "@fullcalendar/core";
 import timeGridPlugin from '@fullcalendar/timegrid';
+import { EventsService } from "@core/services/events.service";
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [ FullCalendarModule, CommonModule],
+  imports: [ FullCalendarModule, CommonModule, HttpClientModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
@@ -16,11 +19,23 @@ export class CalendarComponent {
     initialView: 'timeGridWeek',
     plugins:[timeGridPlugin],
     //dateClick: this.handleDateClick.bind(this),
-    events: [
-      { title: 'Anniversaire Simon', start: '2024-05-08T10:00:00', end: '2024-05-03T12:00:00' },
-      { title: 'Journée portes ouvertes', start: '2024-05-04', allDay: true }    ]
+    events: []
   };
 
+  constructor(private eventsService: EventsService) { }
+
+  ngOnInit(): void {
+    this.loadEvents();
+  }
+
+  loadEvents(): void {
+    this.eventsService.getEvents().subscribe({
+      next: (events) => {
+        this.calendarOptions.events = events; // Update the events
+      },
+      error: (err) => console.error('Failed to load events', err)
+    });
+  }
   handleDateClick(arg: any) {
     alert('Date clicked: ' + arg.dateStr);
   }
