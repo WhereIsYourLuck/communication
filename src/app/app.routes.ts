e54b80893d871b1ab73d752de2a38a4157cc45f7
rlesage71 <97740871+rlesage71@users.noreply.github.com>
2024-05-02 14:05:17 +0200

    Ajout module calendrier + evenements test en dur dans le code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CalendarComponent } from "./pages/calendar/components/calendar/calendar.component";
 
-export const routes: Routes = [{ path: '', component: DashboardComponent }];
+export const routes: Routes = [{ path: '', component: CalendarComponent }];
